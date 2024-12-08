a2ff5454550710c1e9c66537658e490dbc7fd608
Jakub Fałek <jakub2002pl@wp.pl>
2024-12-08 21:28:01 +0100

    Initial commit

---
+++ src/main/java/com/jf/coachingohub/CoachingohubApplication.java
@@ -0,0 +1,13 @@
+package com.jf.coachingohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachingohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachingohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jf/coachingohub/CoachingohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jf.coachingohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachingohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
